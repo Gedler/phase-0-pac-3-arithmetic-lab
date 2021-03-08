8642ff2510d597bce053cc2058a79fcf4569e0a4
Gedler <gedler.jeanpierre@stonybrook.edu>
2021-03-08 15:55:50 +0800

    Completed assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+
+function add(number1,number2) {
+    return number1+number2;
+}
+function subtract(number2,number1) {
+    return number2-number1;
+}
+function multiply(number1,number2) {
+    return number1*number2;
+}
+function divide(number2, number1) {
+    return number2/number1;
+}
+function increment(n) {
+    return n+=1;
+}
+function decrement(n) {
+    return n-=1;
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+    
+}
+function preserveDecimal(n) {
+    return parseFloat(n, 10);
+    
+}
+
+add(1210,8);
+subtract(0,-425);
+multiply;
+divide;
+increment;
+decrement;
+makeInt;
+preserveDecimal;
+n;
